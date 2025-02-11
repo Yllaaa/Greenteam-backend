57ec56e8ef75f93a9ff33e5d1ba7651087c4e0b9
احمد حسام محمد فرج <ahmdhsammfarge@gmail.com>
2025-02-11 14:49:47 +0200

    feat: pages repo & service

--- src/modules/pages/pages.repository.ts
+++ src/modules/pages/pages.repository.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { DrizzleService } from '../db/drizzle.service';
+import { pages, pagesContacts, pagesLikes } from '../db/schemas/schema';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
-export class PagesRepository {}
+export class PagesRepository {
+    constructor(
+        private readonly drizzleService: DrizzleService
+    ) { }
+
+    async createPage(page: any){
+        return await this.drizzleService.db.insert(pages).values(page)
+    }
+
+    async getPage(id: string){
+        return await this.drizzleService.db.query.pages.findFirst({
+            where: eq(pages.id, id),
+            with: {
+                owner: {
+                    columns: {
+                        fullName: true,
+                        avatar: true
+                    }
+                },
+                topic: true,
+                contacts: true,
+                likes: {
+                    columns: {},
+                    with:{
+                        user: {
+                            columns: {
+                                fullName: true,
+                                avatar: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    }
+
+    async addPageContact(contact: any){
+        return await this.drizzleService.db.insert(pagesContacts).values(contact)
+    }
+
+    async addPageLike(page_id: string, user_id: string){
+        return await this.drizzleService.db.insert(pagesLikes).values({
+            page_id: page_id,
+            user_id: user_id
+        })
+    }
+}
--- src/modules/pages/pages.service.ts
+++ src/modules/pages/pages.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PagesRepository } from './pages.repository';
+import { PageDto } from './dto/pages.dto';
+import { PageContactDto } from './dto/page-contact.dto';
 
 @Injectable()
-export class PagesService {}
+export class PagesService {
+    constructor(
+        private readonly pagesRepository: PagesRepository
+    ){}
+
+    async createPage(page: PageDto, user: any){
+        page.owner_id = user.id
+        return await this.pagesRepository.createPage(page)
+    }
+
+    async getPage(id: string){
+        return await this.pagesRepository.getPage(id)
+    }
+
+    async addPageContact(contact: PageContactDto, page_id: string){
+        contact.page_id = page_id
+        return await this.pagesRepository.addPageContact(contact)
+    }
+
+    async addPageLike(page_id: string, user: any){
+        return await this.pagesRepository.addPageLike(page_id, user.id)
+    }
+}
