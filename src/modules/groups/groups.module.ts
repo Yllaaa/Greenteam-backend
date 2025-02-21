59b76d5e96b6b72ad0988c6d0da9c276f685d456
dohaelsawy <dohaelsawy18@gmail.com>
2025-02-22 01:14:46 +0200

    feat: implement group members can post

--- src/modules/groups/groups.module.ts
+++ src/modules/groups/groups.module.ts
@@ -3,10 +3,12 @@ import { GroupsController } from './groups.controller';
 import { GroupsRepository } from './groups.repository';
 import { GroupsService } from './groups.service';
 import { GroupMembersModule } from './group-members/group-members.module';
+import { GroupPostsModule } from './posts/posts.module';
+
 
 @Module({
     controllers: [GroupsController],
     providers: [GroupsService, GroupsRepository],
-    imports: [GroupMembersModule],
+    imports: [GroupMembersModule, GroupPostsModule],
 })
 export class GroupsModule {}
---
+++ src/modules/groups/posts/posts.controller.ts
@@ -0,0 +1,32 @@
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    Post,
+    Query,
+    Req,
+    UseGuards,
+} from '@nestjs/common';
+import { GroupPostsService } from './posts.service';
+import { CreatePostDto } from '../../shared-modules/posts/posts/dto/create-post.dto';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
+
+@Controller('groups')
+@UseGuards(JwtAuthGuard)
+export class GroupPostsController {
+    constructor(
+        private readonly groupPostsService: GroupPostsService,
+    ) { }
+
+    @Post(':groupId/publish-group-post')
+    async createGroupPost(
+        @Body() createPostDto: CreatePostDto,
+        @Req() req,
+        @Param('groupId') groupId: string) {
+
+        const groupMemberId: string = req.user.id;
+
+        return this.groupPostsService.createPost(createPostDto, groupId, groupMemberId);
+    }
+}
