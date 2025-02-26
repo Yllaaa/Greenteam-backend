18b4a6385a4263e4c1d59c1c789c4c0b106876c2
dohaelsawy <dohaelsawy18@gmail.com>
2025-02-26 09:40:54 +0200

    feat: implement get-posts for group - v1

--- src/modules/groups/posts/posts.controller.ts
+++ src/modules/groups/posts/posts.controller.ts
@@ -11,12 +11,15 @@ import {
 import { GroupPostsService } from './posts.service';
 import { CreatePostDto } from '../../shared-modules/posts/posts/dto/create-post.dto';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
+import { PostsService } from '../../shared-modules/posts/posts/posts.service';
+import { GetPostsDto } from '../../shared-modules/posts/posts/dto/get-posts.dto';
 
 @Controller('groups')
 @UseGuards(JwtAuthGuard)
 export class GroupPostsController {
     constructor(
         private readonly groupPostsService: GroupPostsService,
+        private readonly postsService: PostsService,
     ) { }
 
     @Post(':groupId/publish-post')
@@ -28,5 +31,14 @@ export class GroupPostsController {
         const groupMemberId: string = req.user.id;
 
         return this.groupPostsService.createPost(createPostDto, groupId, groupMemberId);
+    }
+
+    @Get(':groupId/get-posts')
+    async getGroupPosts(
+        @Req() req,
+        @Query() getPostDto: GetPostsDto,
+    ) {
+        const userId: string = req.user.id;
+        return this.postsService.getPosts(getPostDto, userId);
     }
 }
