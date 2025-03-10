ead34c146c22d42767bc7a76b646d36d18c3a5ab
mohamed yasser <mohamed3lalily@gmail.com>
2025-03-10 17:36:39 +0200

    fix: fixed drizzle migrations issue

--- drizzle/0004_clammy_landau.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "posts" ADD COLUMN "group_id" uuid;--> statement-breakpoint
-ALTER TABLE "posts" ADD CONSTRAINT "posts_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "public"."groups"("id") ON DELETE no action ON UPDATE no action;
--- drizzle/0005_clumsy_synch.sql
+++
@@ -1,1 +0,0 @@
-ALTER TYPE "public"."action" ADD VALUE 'sign' BEFORE 'challenge';
